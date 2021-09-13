412def8f270d0493ac34cb82cc5ec5b318d6110e
Jeff Snyder <forexsnyder@gmail.com>
2021-09-12 22:05:09 -0700

    monolith

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ variable subnet_cidr_blocks {}
 
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
+variable public_key {}
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_blocks
     tags = {
@@ -41,8 +43,73 @@ resource "aws_internet_gateway" "myapp-igw" {
         Name: "${var.env_prefix}-igw"
     }
 }
-
+#Associate route Subnet to Route Table
 resource "aws_route_table_association" "a-rtb-subnet" {
     subnet_id = aws_subnet.myapp-subnet-1.id
     route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        #cidr_blocks = ["0.0.0.0./0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        #cidr_blocks = ["0.0.0.0./0"]
+        prefix_list_ids = []
+    }
+        tags = {
+        Name: "${var.env_prefix}-sg"
+    }
+}
+resource "aws_ami" "latest-amazon-linux-image"{
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name ="name"
+        value = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = var.public_key
+  
+}
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true 
+    key_name = aws_key_pair.ssh-key.key_name
+
+    user_data = <<EOF
+                    #!/bin/bash
+                    sudo yum update -y && sudo yum install docker -y
+                    sudo systemctl start docker
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:80 nginx
+                EOF
+    tags {
+        Name = "${var.env_prefix}-server"
+    }
 }
